50ec9f05083821bedb81c80ad1aed93dbc50be15
DaffaMalik12 <dmalikakram@gmail.com>
2024-01-04 18:14:48 +0700

    perubahan grid

--- src/components/cardSkill.jsx
+++ src/components/cardSkill.jsx
@@ -1,37 +1,30 @@
 function CardSkill() {
   return (
     <>
-      <div className="lg:flex flex-wrap gap-6 justify-evenly ml-20 lg:mt-0 -mt-5 ">
-        <div className=" lg:w-52 lg:h-32 h-32 w-80 -ml-20 lg:-ml-0 lg:mr-0 shadow-2xl bg-white rounded-md mb-6 lg:mb-0">
-          <img
-            className=" lg:w-28 lg:mt-2 lg:mx-auto w-24 pt-3 lg:pt-0 mx-auto mt-8 rounded-md"
-            src="/img/html.png"
-            alt=""
-          />
-        </div>
-        <div className="lg:w-52 lg:h-32 h-32 lg:ml-10 w-80 -ml-20 lg:mr-0 shadow-2xl bg-white rounded-md mb-6 lg:mb-0">
-          <img
-            className=" lg:w-24 lg:mx-auto lg:mt-4 w-24 pt-3 mx-auto  lg:pt-0 rounded-md"
-            src="/img/css.png"
-            alt=""
-          />
-        </div>
+      <div className=" shadow-2xl lg:w-32 bg-white rounded-md ">
+        <img className="  mx-auto rounded-md" src="/img/html.png" alt="" />
       </div>
-      <div className="lg:flex flex-wrap gap-6 lg:ml-auto lg:mr-28 lg:-mt-28 justify-evenly ">
-        <div className="lg:w-52 lg:h-32  h-32 w-80 -ml-0 shadow-2xl lg:-ml-0 lg:mr-0 bg-white rounded-md mb-6 lg:mb-0">
-          <img
-            className=" lg:w-24 lg:mx-auto lg:mt-4 w-24 pt-3 mx-auto  lg:pt-0 rounded-md"
-            src="/img/js.png"
-            alt=""
-          />
-        </div>
-        <div className="lg:w-52 lg:h-32 h-32 w-80 -ml-0 lg:ml-10 lg:mr-0  shadow-2xl bg-white rounded-md mb-6 lg:mb-0">
-          <img
-            className=" lg:w-24 lg:mx-auto lg:mt-5 w-24 pt-5 mx-auto  lg:pt-0 rounded-md"
-            src="/img/react.png"
-            alt=""
-          />
-        </div>
+      <div className="shadow-2xl lg:w-32 bg-white rounded-md ">
+        <img
+          className=" w-40 pt-2 lg:w-28 lg:pt-2 lg:mx-auto mx-auto rounded-md"
+          src="/img/css.png"
+          alt=""
+        />
+      </div>
+
+      <div className=" shadow-2xl lg:w-32  bg-white rounded-md ">
+        <img
+          className="  lg:w-24 lg:mx-auto mx-auto rounded-md"
+          src="/img/js.png"
+          alt=""
+        />
+      </div>
+      <div className=" lg:w-32  shadow-2xl bg-white rounded-md  ">
+        <img
+          className=" w-40  lg:w-24 lg:pt-1 pt-4 lg:mx-auto mx-auto rounded-md"
+          src="/img/react.png"
+          alt=""
+        />
       </div>
     </>
   );
--- src/components/skill.jsx
+++ src/components/skill.jsx
@@ -10,16 +10,18 @@ function ComponentSkill() {
   return (
     <Slide direction="right">
       <section className="lg:w-screen w-screen lg:p-5 p-10 lg:mt-10 mt-10 bg-slate-300 overflow-x-hidden">
-        <div className="container lg:ml-20 lg:mt-10 -ml-5">
-          <div className="flex flex-wrap">
+        <div className="container lg:px-20 lg:py-10">
+          <div className="flex flex-col justify-around lg:flex-row ">
             <div className="skillset">
               <h1 className="text-2xl font-bold text-cyan-400">{skillset}</h1>
-              <h2 className="w-80 text-justify text-xl font-semibold">
+              <h2 className="lg:w-96 w-full text-justify text-xl font-semibold">
                 {creative}
               </h2>
-              <p className="max-w-xs text-justify">{crafted}</p>
+              <p className="lg:w-96 w-full text-justify">{crafted}</p>
             </div>
-            <CardSkill />
+            <div className="grid grid-cols-2 gap-5 lg:w-1/3 mt-5">
+              <CardSkill />
+            </div>
           </div>
         </div>
       </section>
